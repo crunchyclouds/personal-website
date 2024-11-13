003c3ddbd49858cf14fe8dcf73c98971f1874400
Mihir Kalvakaalva <mihir.38k@gmail.com>
2024-11-13 10:01:30 -0600

    More experiences added

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
-import requests
 from PIL import Image
+from pathlib import Path
 
 #loading images etc in
-img_double_helix = Image.open(r"https://github.com/crunchyclouds/personal-website/blob/main/images/double%20helix.webp")
+img_engineering_showcase = Image.open(r"C:\Users\mihir\Programs\Documents\GitHub\personal-website\images\engineering_showcase.jfif")
+img_xerion_group_photo = Image.open(r"C:\Users\mihir\Programs\Documents\GitHub\personal-website\images\xerion_group_photo.jfif")
+img_chemistry_open_house = Image.open(r"C:\Users\mihir\Programs\Documents\GitHub\personal-website\images\chemistry_open_house.jpg")
+
 st.set_page_config(page_title="Mihir's Webpage", page_icon=":ogre", layout="wide")
 
 #homepage header
 st.subheader("Hey, I'm Mihir! :wave:")
 st.title("I'm a materials scientist studying at Texas A&M University.")
-st.write("I love sharing my passions and incorporating my interest for computational methods into my research.")
-st.write("[Learn more about me](https://www.linkedin.com/in/mihir-kalvakaalva)")
+st.write("I love learning about chemistry and sharing my love for science with others!")
+st.write("[Contact me via LinkedIn](https://www.linkedin.com/in/mihir-kalvakaalva)")
 
 #my experiences
+
 with st.container():
     st.write("---")
     st.header("Experiences")
@@ -20,8 +24,28 @@ with st.container():
 
     image_column, text_column = st.columns((1,2))
     with image_column:
-        st.image(img_double_helix)
+        st.image(img_xerion_group_photo)
     with text_column:
         st.subheader("Materials Scientist Intern at Xerion Advanced Battery")
         st.write("This past summer, I interned at an R&D Battery company, where I worked on "
                  "exciting topics relating to Li-ion batteries.")
+
+with st.container():
+    image_column, text_column = st.columns((1,2))
+    with image_column:
+        st.image(img_engineering_showcase)
+    with text_column:
+        st.subheader("Project Lead on Vapor Compression Desalination")
+        st.write("In 2024, I worked on a vapor compression desalination project under the "
+                 "advice of Dr. Mark Holtzapple. We made substantial progress on the technology"
+                 "and won 2nd at the Texas A&M Student Research Week.")
+
+with st.container():
+    image_column, text_column = st.columns((1,2))
+    with image_column:
+        st.image(img_chemistry_open_house)
+    with text_column:
+        st.subheader("Science Chair of SASE Texas A&M Chapter")
+        st.write("In my ongoing role as Science Chair in the Society of Asian Scientists and Engineers,"
+                 "I get to fulfill my interest in spreading interest in science at events like the A&M"
+                 "Chemistry Open House")
